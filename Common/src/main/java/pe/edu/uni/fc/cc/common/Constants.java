7ea49a8b17b1c648540686e2af804f4eac9937d4
cristian <mcgmesajil@gmail.com>
2026-04-01 15:46:08 -0500

    paquetes MAS

--- Common/src/main/java/pe/edu/uni/fc/cc/common/Constants.java
+++ Common/src/main/java/pe/edu/uni/fc/cc/common/Constants.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pe.edu.uni.fc.cc.substitution;
+package pe.edu.uni.fc.cc.common;
 
 /**
  *
  * @author alumno
  */
-public class Constans {
+public class Constants {
     
 }
