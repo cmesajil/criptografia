22875a0211c6c15864c7ffde33857dcd298ab470
cristian <mcgmesajil@gmail.com>
2026-04-01 15:32:54 -0500

    paquetes MAS

--- Common/src/main/java/pe/edu/uni/fc/cc/substitution/Constans.java
+++ Common/src/main/java/pe/edu/uni/fc/cc/substitution/Constans.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pe.edu.uni.fc.cc.subtitution.service;
+package pe.edu.uni.fc.cc.substitution;
 
 /**
  *
  * @author alumno
  */
-public class CaesarCipherService {
+public class Constans {
     
 }
--- Substitution/src/main/java/pe/edu/uni/fc/cc/substitution/Constans.java
+++ Substitution/src/main/java/pe/edu/uni/fc/cc/substitution/Constans.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pe.edu.uni.fc.cc.subtitution.App;
+package pe.edu.uni.fc.cc.substitution;
 
 /**
  *
  * @author alumno
  */
-public class MainCeasarCipher {
+public class Constans {
     
 }
